624b47bdfb20c7a5c72ffdfb5a4d07a2ff18cef3
zaher hamadeh <hamadehzaher0@gmail.com>
2015-03-28 18:56:14 +0200

    Revert "deleting other unnecessary files"
    This reverts commit b42352596ff9e14cb10adee514bda82f9d55f5e9.

---
+++ Prototype4/freq/freqtable.cpp
@@ -0,0 +1,69 @@
+#include "freqtable.h"
+#define FH 35
+#define FF 38
+
+Freqtable * Freqtable::single=0;
+
+Freqtable::Freqtable(int minValue, int maxValue)
+{
+    _minFreq=minValue;
+    _maxFreq=maxValue;
+}
+
+int Freqtable::min()
+{
+    return _minFreq;
+}
+
+int Freqtable::max()
+{
+    return _maxFreq;
+}
+
+int Freqtable::find(double freq)
+{
+    int j,i=j=NFT/2;
+    while(FREQTABLE[i]>=freq||FREQTABLE[i+1]<freq)
+    {
+        j/=2;
+        if(j==0)
+            j=1;
+        if(freq>FREQTABLE[i])
+        {
+            i+=j;
+        }
+        else
+        {
+            i-=j;
+        }
+    }
+    if(freq-FREQTABLE[i]<FREQTABLE[i+1]-freq)
+        return i;
+    else
+        return i+1;
+}
+
+void Freqtable::setMax(int maxValue)
+{
+    _maxFreq=maxValue;
+}
+
+void Freqtable::setMin(int minValue)
+{
+    _minFreq=minValue;
+}
+
+void Freqtable::set(int minValue, int maxValue)
+{
+    _minFreq=minValue;
+    _maxFreq=maxValue;
+}
+
+
+
+Freqtable *Freqtable::getInstance()
+{
+    if(single==0)
+        single=new Freqtable(FH,FF);
+    return single;
+}
