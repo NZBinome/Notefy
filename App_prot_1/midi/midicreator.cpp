36542009f042f41ddfb04127bbd1a0fdcee620f9
zaher hamadeh <hamadehzaher0@gmail.com>
2015-03-28 18:23:38 +0200

    newAdd

---
+++ App_prot_1/midi/midicreator.cpp
@@ -0,0 +1,121 @@
+#include "note.h"
+#include "midicreator.h"
+#include <stdio.h>
+#include <fstream>
+
+using namespace std;
+
+MidiCreator::MidiCreator(int fs)
+{
+    double t=1.0/fs;        //un echantillon fait t seconde
+    _fdt=t*2*_dt;           //une note de longueur t fait _fdt tick
+    _inst=1;
+    //_name=0;
+}
+
+void MidiCreator::addNote(Note n)
+{
+    if(_n.size()==0)
+    {
+        _n.push_back(n);
+        return;
+    }
+
+    if(_n.back().n()==n.n())
+    {
+        if(_n.back().v()>=n.v())
+            _n.back().inc_d(n.d());
+    }
+    else
+    {
+        _n.push_back(n);
+    }
+}
+
+int MidiCreator::sizeData(char *&c)
+{
+    //printf("size %d\n",_n.size());
+    c=new char[3+_n.size()*14];
+    int sd=3;
+    c[0]=0;
+    c[1]=192;
+    c[2]=_inst;
+    for(int i=0;i<_n.size();++i)
+    {
+        int s;
+        memcpy(c+sd,((Note)_n[i]).toMidiMessage(s,_fdt,0),11);
+        sd+=s;
+    }
+    return sd;
+}
+
+void MidiCreator::creerMidiFile(char *name)
+{
+    fstream f;
+    _name=name;
+    f.open(_name,ios::out|ios::binary);
+    //on ecrit le header du fichier
+    char ch[]="MThd";
+    f.write(ch,4*sizeof(char));
+
+    //la taille du header
+    int data=6;
+    unsigned char c;
+    c=(unsigned char)((data>>24)&0xFF);
+    f.write((char*)&c,sizeof(char));
+    c=(unsigned char)((data>>16)&0xFF);
+    f.write((char*)&c,sizeof(char));
+    c=(unsigned char)((data>>8)&0xFF);
+    f.write((char*)&c,sizeof(char));
+    c=(unsigned char)((data)&0xFF);
+    f.write((char*)&c,sizeof(char));
+
+    //le format du fichier
+    data=0;
+    c=(unsigned char)((data>>8)&0xFF);
+    f.write((char*)&c,sizeof(char));
+    c=(unsigned char)((data)&0xFF);
+    f.write((char*)&c,sizeof(char));
+
+    //le nombre de track
+    data=1;
+    c=(unsigned char)((data>>8)&0xFF);
+    f.write((char*)&c,sizeof(char));
+    c=(unsigned char)((data)&0xFF);
+    f.write((char*)&c,sizeof(char));
+
+    //le nombre de tick par noire
+    data=_dt;
+    c=(unsigned char)((data>>8)&0xFF);
+    f.write((char*)&c,sizeof(char));
+    c=(unsigned char)((data)&0xFF);
+    f.write((char*)&c,sizeof(char));
+
+    char ch2[]="MTrk";
+    f.write(ch2,4*sizeof(char));
+
+    //la taille du MIDI data
+    char *d=0;
+    data=sizeData(d)+4;
+    c=(unsigned char)((data>>24)&0xFF);
+    f.write((char*)&c,sizeof(char));
+    c=(unsigned char)((data>>16)&0xFF);
+    f.write((char*)&c,sizeof(char));
+    c=(unsigned char)((data>>8)&0xFF);
+    f.write((char*)&c,sizeof(char));
+    c=(unsigned char)((data)&0xFF);
+    f.write((char*)&c,sizeof(char));
+
+    f.write(d,(data-4)*sizeof(char));
+
+    if(d!=0)
+        delete []d;
+
+    unsigned char eot[4] = {0, 0xff, 0x2f, 0x00};
+    f.write((char*)eot,sizeof(char));
+    f.write((char*)eot+1,sizeof(char));
+    f.write((char*)eot+2,sizeof(char));
+    f.write((char*)eot+3,sizeof(char));
+
+    f.close();
+}
--- Prototype4/Prototype4.pro
+++ Prototype4/Prototype4.pro
@@ -4,23 +4,46 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += main.cpp \
-    wavFormat/diviseur.cpp \
-    wavFormat/wavread.cpp \
-    signal/complex.cpp \
-    signal/melody.cpp \
-    signal/signal.cpp \
-    midi/midicreator.cpp \
-    midi/note.cpp \
-    freq/freqtable.cpp
+    ../App_prot_1/wavFormat/diviseur.cpp \
+    ../App_prot_1/wavFormat/wavread.cpp \
+    ../App_prot_1/signal/complex.cpp \
+    ../App_prot_1/signal/melody.cpp \
+    ../App_prot_1/signal/signal.cpp \
+    ../App_prot_1/midi/midicreator.cpp \
+    ../App_prot_1/midi/musicinstrument.cpp \
+    ../App_prot_1/midi/note.cpp \
+    ../App_prot_1/libaiff/aifx.c \
+    ../App_prot_1/libaiff/extended.c \
+    ../App_prot_1/libaiff/float32.c \
+    ../App_prot_1/libaiff/g711.c \
+    ../App_prot_1/libaiff/iff.c \
+    ../App_prot_1/libaiff/libaiff.c \
+    ../App_prot_1/libaiff/lpcm.c \
+    ../App_prot_1/libaiff/pascal.c \
+    ../App_prot_1/libaiff/z_util.c \
+    ../App_prot_1/freq/freqtable.cpp \
+    ../App_prot_1/audioread/aiffread.cpp \
+    ../App_prot_1/audioread/audioread.cpp \
+    ../App_prot_1/audiomidiconverter/z_audiomidiconverter.cpp
+
 
 HEADERS += \
-    wavFormat/diviseur.h \
-    wavFormat/wavread.h \
-    signal/complex.h \
-    signal/melody.h \
-    signal/signal.h \
-    midi/midicreator.h \
-    midi/mynote.h.autosave \
-    midi/note.h \
-    freq/freqtable.h
+    ../App_prot_1/wavFormat/diviseur.h \
+    ../App_prot_1/wavFormat/wavread.h \
+    ../App_prot_1/signal/complex.h \
+    ../App_prot_1/signal/melody.h \
+    ../App_prot_1/signal/signal.h \
+    ../App_prot_1/midi/midicreator.h \
+    ../App_prot_1/midi/musicinstrument.h \
+    ../App_prot_1/midi/note.h \
+    ../App_prot_1/libaiff/private.h \
+    ../App_prot_1/libaiff/z_util.h \
+    ../App_prot_1/libaiff/libaiff/config.h \
+    ../App_prot_1/libaiff/libaiff/endian.h \
+    ../App_prot_1/libaiff/libaiff/libaiff.h \
+    ../App_prot_1/freq/freqtable.h \
+    ../App_prot_1/audioread/aiffread.h \
+    ../App_prot_1/audioread/audioread.h \
+    ../App_prot_1/audiomidiconverter/z_audiomidiconverter.h
+
 
